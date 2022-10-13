f09279a10ed587fe61ac920f29b1d17b62e305b2
HARSH DEV <98793412+Harshdev625@users.noreply.github.com>
2022-10-13 23:27:46 +0530

    Update Binary_Search.cpp

--- C++/Binary_Search.cpp
+++ C++/Binary_Search.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int binarysearch(int *a, int n, int x)
 {
+//  start variable denotes the starting point in an array.
+//  end variable denotes the end point of array.
+//  mid variable to find the mid value between them.
     int start = 0;
     int end = n - 1;
     int mid;
@@ -25,13 +28,16 @@ int binarysearch(int *a, int n, int x)
 }
 int main()
 {
+    // Input the size of the array
     int n;
     cin >> n;
+    // Create an Array of N integers
     int *a = new int[n];
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
+    // Input the value of x you want to search 
     int x;
     cin >> x;
     cout << binarysearch(a, n, x);
